db204c797f2c196bbcdca1b5e216d7b812aceca1
Ando Yasushi <andyjpn@gmail.com>
2012-08-21 14:28:37 +0900

    i18n

--- js/irc.js
+++ js/irc.js
@@ -1,7 +1,7 @@
 if (typeof(IRC) == 'undefined' || !IRC.App) throw 'load error';
 
 window.addEventListener('load', function(evt) {
-  IRC.Settings.clearAllStorage();
+  //IRC.Settings.clearAllStorage();
   //IRC.Settings.setupTestData();
   IRC.App.start();
 });
