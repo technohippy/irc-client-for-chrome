d527d8a6ba1b45b3e71a93e913bdbdd3ef677d8b
Ando Yasushi <andyjpn@gmail.com>
2012-08-22 21:40:22 +0900

    readme

--- js/irc.util.js
+++ js/irc.util.js
@@ -2,8 +2,11 @@ if (typeof(IRC) == 'undefined') var IRC = {};
 
 IRC.Util = {};
 IRC.Util.messageToHTML = function(message) {
+  var htmlMessage = message.text.replace('<', '&lt;');
+  //htmlMessage = htmlMessage.replace(/https?:\/\/[a-zA-Z0-9_\.\/%]+/, '<a href="\\&" target="_blank">\\&</a>');
+  //htmlMessage += '<a href="http://google.com" target="_blank">google</a>';
   return '<div class="line"><span class="sender">' + message.sender + '</span>' + 
-    '<span class="text">' + message.text.replace('<', '&lt;') + '</span>' +
+    '<span class="text">' + htmlMessage + '</span>' +
     '<span class="timestamp">' + message.timestamp.hm() + '</span></div>';
 };
 IRC.Util.isBlank = function(val) {
