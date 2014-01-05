d8b9f28c864dd948edd91edfe97e046ac1cd993b
Ando Yasushi <andyjpn@gmail.com>
2014-01-06 00:53:16 +0900

    make panes resizable

--- js/irc.js
+++ js/irc.js
@@ -1,19 +1,42 @@
 if (typeof(IRC) == 'undefined' || !IRC.App) throw 'load error';
 
 var ircApp;
+var mouseMoving = null;
 window.addEventListener('load', function(evt) {
   //IRC.Settings.clearAllStorage();
   //IRC.Settings.setupTestData();
   ircApp = IRC.App.start();
 
-  /*
-  $('.horizontal-separator').mousedown(function(evt) {
-    console.log(evt);
+  $('.left-pane .horizontal-separator').mousedown(function(evt) {
+    mouseMoving = 'left-pane';
   });
-  $('.horizontal-separator').mouseup(function(evt) {
-    console.log(evt);
+  $('.right-pane .horizontal-separator').mousedown(function(evt) {
+    mouseMoving = 'right-pane';
   });
-  */
+  $('.container .vertical-separator').mousedown(function(evt) {
+    mouseMoving = 'container';
+  });
+  $('.container').mousemove(function(evt) {
+    if (mouseMoving != null) {
+      if (mouseMoving == 'left-pane') {
+        var height = parseInt($('.container').css('height'));
+        $('.logs').css('height', (height - evt.pageY - 2) + 'px');
+      }
+      else if (mouseMoving == 'right-pane') {
+        var height = parseInt($('.container').css('height'));
+        $('.channels').css('height', (height - evt.pageY - 2) + 'px');
+      }
+      else if (mouseMoving == 'container') {
+        var width = parseInt($('.container').css('width'));
+        $('.right-pane').css('width', (width - evt.pageX - 1) + 'px');
+      }
+    }
+  });
+  $('.container').mouseup(function(evt) {
+    if (mouseMoving) {
+      mouseMoving = null;
+    }
+  });
 });
 /*
 window.addEventListener('unload', function(evt) {
